a519273da928eae73c7af3d4af5995703198c286
Captain <zeasts@aol.com>
2022-03-20 05:00:52 +0530

    First Successfull Build

--- components/App.js
+++ components/App.js
@@ -1,42 +1,18 @@
-/*
- * Learning Isomorphic Web Application Development
- * Copyright © 2015 Konstantin Tarkus, Packt Publishing
- * All rights reserved.
- */
-
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import moment from 'moment';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { time: null };
-  }
-
-  componentDidMount() {
-    this.tick();
-    this.interval = setInterval(this.tick.bind(this), 200);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  tick() {
-    this.setState({ time: new Date() });
-  }
-
-  render() {
-    const time = this.state.time;
-    const timeString = time && moment(time).format('h:mm:ss a');
+const App =() => {
+const [timeString, settimeString] = useState(()=>{
+  let currDate = new Date()
+  return currDate.toDateString()
+})
     return (
       <div>
         <h1>Sample Application</h1>
         <p>Current date and time is {timeString}</p>
       </div>
     );
-  }
+  
 
 }
 
